7db8105a2bf5e5d4fd52886c3b866bf996e20810
Carlos Roberto Arias Arevalo <arias@spu.edu>
2022-04-04 11:27:00 -0700

    Vector is done
    Dunno if it works though, have to make main

---
+++ vector.cpp
@@ -0,0 +1,166 @@
+//
+// Created by Carlos R. Arias on 4/4/22.
+//
+
+#include "vector.h"
+
+#include <cmath>
+#include <sstream>
+
+using std::stringstream;
+
+Vector::Vector(size_t size) {
+    if (size == 0){
+        _data = nullptr;
+        _size = 0;
+    }else{
+        _size = size;
+        _data = new double[_size];
+        for (int i = 0; i < _size; ++i) {
+            _data[i] = 0.0;
+        }
+    }
+}
+
+Vector::Vector(double *data, size_t size) {
+    if (size == 0){
+        _data = nullptr;
+        _size = 0;
+    }else{
+        _size = size;
+        _data = new double[_size];
+        for (int i = 0; i < _size; ++i) {
+            _data[i] = data[i];
+        }
+    }
+}
+
+Vector::Vector(const Vector &other) {
+    if (other._size == 0){
+        _data = nullptr;
+        _size = 0;
+    }else{
+        _size = other._size;
+        _data = new double[_size];
+        for (int i = 0; i < _size; ++i) {
+            _data[i] = other._data[i];
+        }
+    }
+}
+
+Vector::~Vector() {
+    if (_data != nullptr)
+        delete[] _data;
+}
+
+const Vector &Vector::operator=(const Vector &rhs) {
+    if (this == &rhs)
+        return *this;
+    if (rhs._size == 0){
+        delete[] _data;
+        _data = nullptr;
+        _size = 0;
+        return *this;
+    }
+    if (rhs._size == _size){
+        for (int i = 0; i < _size; ++i) {
+            _data[i] = rhs._data[i];
+        }
+    }else{
+        delete[] _data;
+        _size = rhs._size;
+        _data = new double[_size];
+        for (int i = 0; i < _size; ++i) {
+            _data[i] = rhs._data[i];
+        }
+    }
+    return *this;
+}
+
+const Vector Vector::Sum(const Vector &rhs) const {
+    if (_size != rhs._size){
+        return Vector(0);
+    }
+    Vector retVal(_size);
+    for (int i = 0; i < _size; ++i) {
+        retVal._data[i] = _data[i] + rhs._data[i];
+    }
+    return retVal;
+}
+
+const Vector Vector::Sub(const Vector &rhs) const {
+    if (_size != rhs._size){
+        return Vector(0);
+    }
+    Vector retVal(_size);
+    for (int i = 0; i < _size; ++i) {
+        retVal._data[i] = _data[i] - rhs._data[i];
+    }
+    return retVal;
+}
+
+bool Vector::Equals(const Vector &rhs) const {
+    if (_size != rhs._size)
+        return false;
+    for (int i = 0; i < _size; ++i) {
+        if (_data[i] != rhs._data[i])
+            return false;
+    }
+    return true;
+}
+
+double Vector::Distance(const Vector &rhs) const {
+    if (_size != rhs._size)
+        return -1;
+    double sum = 0;
+    for (int i = 0; i < _size; ++i) {
+        sum += pow(_data[i] - rhs._data[i], 2);
+    }
+    return sqrt(sum);
+}
+
+string Vector::ToString() const {
+    stringstream ss;
+    ss << "[";
+    for (int i = 0; i < _size; ++i) {
+        if (i != _size - 1)
+            ss << _data[i] << ", ";
+        else
+            ss << _data[i] << "]";
+    }
+    return ss.str();
+}
+
+ostream &Vector::Write(ostream &output) const {
+    output << _size << " ";
+    for (int i = 0; i < _size; ++i) {
+        output << _data[i] << " ";
+    }
+    return output;
+}
+
+istream &Vector::Read(istream &input)  {
+    size_t inputSize;
+    input >> inputSize;
+    if (input.fail())
+        return input;
+    if (_size != inputSize){
+        delete[] _data;
+        _size = inputSize;
+        _data = new double [_size];
+        for (int i = 0; i < _size; ++i) {
+            input >> _data[i];
+            if (input.fail())
+                return input;
+        }
+    }
+    return input;
+}
+
+size_t Vector::GetDimension() const {
+    return _size;
+}
+
+bool Vector::IsValid() const {
+    return _size != 0;
+}
---
+++ vector.h
@@ -0,0 +1,40 @@
+//
+// Created by Carlos R. Arias on 4/4/22.
+//
+
+#ifndef VECTOR_S22_VECTOR_H
+#define VECTOR_S22_VECTOR_H
+
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::ostream;
+using std::istream;
+
+class Vector {
+private:
+    size_t _size;
+    double* _data;
+public:
+    Vector(size_t size);
+    Vector(double* data, size_t size);
+    Vector(const Vector& other);
+    ~Vector();
+    const Vector& operator=(const Vector& rhs);
+
+    const Vector Sum(const Vector& rhs)const;
+    const Vector Sub(const Vector& rhs)const;
+    bool Equals(const Vector& rhs)const;
+    double Distance(const Vector& rhs)const;
+
+    string ToString()const;
+    ostream& Write(ostream& output)const;
+    istream& Read(istream& input);
+
+    size_t GetDimension()const;
+    bool IsValid()const;
+};
+
+
+#endif //VECTOR_S22_VECTOR_H
